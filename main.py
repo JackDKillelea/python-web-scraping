b6d369711529a5bd7f92447cdeb7a579a763b971
Jack Killelea <42438278+JackDKillelea@users.noreply.github.com>
2024-11-08 16:58:48 +0000

    Added local database
    - Added a local db and used sql to read and write data
    
    - Kept solution for writing to file and writing to db in for future reference.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
 import selectorlib
 import time
+import sqlite3
 from send_email import email
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
+connection = sqlite3.connect("data.db")
 
 def scrape(url):
     response = requests.get(url)
@@ -15,21 +17,44 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
-def send_email(extracted):
-    email(extracted, "utf-8")
-
 def store_data(data):
     with open("data.txt", "a") as file:
         file.write(f"{data}\n")
+
+def read_db(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    print(rows)
+    return rows
+
+def store_data_in_db(extracted):
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
+    print("Successfully stored data")
 
 if __name__ == '__main__':
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
         print(extracted)
+
         if extracted.lower() != "no upcoming tours":
-            with open("data.txt") as data:
-                if f"{extracted}\n" not in data:
-                    store_data(extracted)
-                    send_email(extracted)
-        time.sleep(60)
+            # Store data using local database
+            content = read_db(extracted)
+            if not content:
+                store_data_in_db(extracted)
+                email(extracted, "utf-8")
+
+            # Store data using local file
+            # with open("data.txt") as data:
+            #     if f"{extracted}\n" not in data:
+            #         store_data(extracted)
+            #         send_email(extracted)
+        time.sleep(1)
